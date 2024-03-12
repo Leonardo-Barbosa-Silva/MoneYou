ba1fa03bb3a97197758a9f630b406a87420d83e8
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2024-03-11 21:44:28 -0300

    feat: implementa react hook form e zod para o formulario de busca

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,14 +1,30 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import { SearchFormInputsTypes, searchFormSchema } from "./types";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export function SearchForm() {
+  const { register, handleSubmit } = useForm<SearchFormInputsTypes>({
+    resolver: zodResolver(searchFormSchema),
+    mode: 'onSubmit'
+  });
 
-    return (
-        <SearchFormContainer>
-            <input type="text" placeholder="Busque uma transação"/>
-            <button type="submit">
-                <MagnifyingGlass size={25} /> Buscar
-            </button>
-        </SearchFormContainer>
-    )
+  function handleSearchFormSubmit(data: SearchFormInputsTypes) {
+    console.log(data)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchFormSubmit)}>
+      <input
+        {...register("search")}
+        type="text"
+        placeholder="Busque por transações"
+      />
+
+      <button type="submit">
+        <MagnifyingGlass size={25} /> Buscar
+      </button>
+    </SearchFormContainer>
+  );
 }

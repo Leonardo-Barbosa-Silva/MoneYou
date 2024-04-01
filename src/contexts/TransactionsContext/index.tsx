ccd5f122fe2dc432fa4a142cd39ddf003deebe43
Leonardo Barbosa Silva <leonardobarbosassilva@gmail.com>
2024-04-01 10:08:40 -0300

    feat: contexto com variavel getTransactions

--- src/contexts/TransactionsContext/index.tsx
+++ src/contexts/TransactionsContext/index.tsx
@@ -64,7 +64,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
   useEffect(() => {
     getTransactions();
-  }, []);
+  }, [getTransactions]);
 
   return (
     <TransactionsContext.Provider
